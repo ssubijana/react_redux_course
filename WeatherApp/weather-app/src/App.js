39642a56838e71000265b4a2626c441ba7c428d9
Sara Subijana Gracia <sara.subijana@gmail.com>
2020-04-29 16:47:11 +0200

    Adds the first weather detail component

--- WeatherApp/weather-app/src/App.js
+++ WeatherApp/weather-app/src/App.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { Component } from "react";
+import WeatherLocation from "./components/WeatherLocation";
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather Application!</div>;
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <WeatherLocation></WeatherLocation>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ WeatherApp/weather-app/src/components/WeatherData.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const WeatherData = () => <div>Weather Data</div>;
+
+export default WeatherData;
