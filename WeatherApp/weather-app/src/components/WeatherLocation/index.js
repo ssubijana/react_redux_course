33916c01a8b070297974c13d29a083af48c3e980
Sara Subijana Gracia <sara.subijana@gmail.com>
2020-05-07 18:00:36 +0200

    Adds promise and connects to weather api

--- WeatherApp/weather-app/src/components/WeatherLocation/index.js
+++ WeatherApp/weather-app/src/components/WeatherLocation/index.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { Component } from "react";
 import Location from "./Location";
 import WeatherData from "./WeatherData";
 import { CLOUD, CLOUDY, SUN, RAIN, SNOW, WINDY } from "./../../constants/weathers";
+import { API_KEY, URL_BASE } from "./../../constants/weatherApi";
 
 import "./styles.css";
+
+const location = "Zaragoza,es";
+const api_weather = `${URL_BASE}?q=${location}&appid=${API_KEY}`;
 
 const data = {
   temperature: 5,
@@ -12,11 +16,44 @@ const data = {
   wind: "10 m/s",
 };
 
-const WeatherLocation = () => (
-  <div className="weatherLocationCont">
-    <Location city={"Arganda del Rey"}></Location>
-    <WeatherData data={data} />
-  </div>
-);
+const data2 = {
+  temperature: 15,
+  weatherState: RAIN,
+  humidity: 20,
+  wind: "40 m/s",
+};
+
+class WeatherLocation extends Component {
+  constructor() {
+    super();
+    this.state = {
+      city: "Zaragoza",
+      data: data,
+    };
+  }
+
+  handleOnClick = () => {
+    fetch(api_weather)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => console.log(data));
+
+    this.setState({
+      data: data2,
+    });
+  };
+
+  render() {
+    const { city, data } = this.state;
+    return (
+      <div className="weatherLocationCont">
+        <Location city={city}></Location>
+        <WeatherData data={data} />
+        <button onClick={this.handleOnClick}>Update</button>
+      </div>
+    );
+  }
+}
 
 export default WeatherLocation;
---
+++ WeatherApp/weather-app/src/constants/weatherApi.js
@@ -0,0 +1,2 @@
+export const API_KEY = "f2de5c79307156eaf2d9aaa3b2093221";
+export const URL_BASE = "http://api.openweathermap.org/data/2.5/weather";
