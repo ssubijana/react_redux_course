cd242c6f745933fb480f8d4821062a8f797bd5d9
Sara Subijana Gracia <sara.subijana@gmail.com>
2020-09-02 15:18:51 +0200

    First calculator approximation

--- calc-app/src/App.js
+++ calc-app/src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="react-calculator">
+      <div className="result"></div>
+      <div className="numbers">
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>0</button>
+      </div>
+      <div className="functions">
+        <button>Clear</button>
+        <button>r</button>
+      </div>
+      <div className="math-operations">
+        <button>+</button>
+        <button>-</button>
+        <button>*</button>
+        <button>/</button>
+        <button>=</button>
+      </div>
+    </main>
   );
 }
-
-export default App;
